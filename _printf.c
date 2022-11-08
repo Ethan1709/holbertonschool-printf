819302be9e44775e952af2e43f7fb1c9a6b44daa
Ethan1709 <benyayerethan@gmail.com>
2022-11-08 08:34:19 -0800

    test 1.0

--- _printf.c
+++ _printf.c
@@ -40,13 +40,13 @@ int _printf(const char *format, ...)
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
-			i = i + 1;
 		{
 			if (format[i + 1] == '\0')
 				return (-1);
 			k = print_format(&format[i + 1]);
 			if (k != NULL)
 				len += k(args);
+			i = i + 1;
 		}
 		else
 		{
